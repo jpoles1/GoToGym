2b3bd3590142000148190d70c599337d795b4ca4
Jordan Poles <jpoles1@gmail.com>
2018-03-05 21:02:46 -0500

    We've got more charts!

--- static/charts.js
+++ static/charts.js
@@ -1,6 +1,7 @@
-function generateGauge(divID, gaugeValue){
+var gaugeColorScale = ['#FF0000', '#F97600', '#F6C600', '#60B044']
+function generateGauge(divID, gaugeValue) {
     c3.generate({
-        bindto: divID,
+        bindto: divID + " .plot-box",
         data: {
             columns: [
                 ['% Attendance', gaugeValue]
@@ -8,7 +9,7 @@ function generateGauge(divID, gaugeValue){
             type: 'gauge',
         },
         color: {
-            pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'], // the three color levels for the percentage values.
+            pattern: gaugeColorScale,
             threshold: {
                 values: [30, 60, 90, 100]
             }
@@ -18,26 +19,73 @@ function generateGauge(divID, gaugeValue){
         }
     });
 }
-function generateAttendancePlot(divID, plotData){
+
+function generateMonthlyMissedGauge(divID, missedCount, totalCount) {
     c3.generate({
-        bindto: divID,
+        bindto: divID + " .plot-box",
+        data: {
+            columns: [
+                ['Missed this Month', missedCount]
+            ],
+            type: 'gauge',
+        },
+        gauge: {
+            max: totalCount,
+            label: {
+                format: function (value, ratio) {
+                    return value;
+                },
+            }
+        },
+        color: {
+            pattern: gaugeColorScale.reverse(), // the three color levels for the percentage values.
+            threshold: {
+                values: [30, 60, 90, 100]
+            }
+        },
+        size: {
+            height: 180
+        }
+    });
+}
+
+function generateAttendancePlot(divID, plotData) {
+    c3.generate({
+        bindto: divID + " .plot-box",
         data: {
             x: 'Time',
             //March 2, 2018 at 10:08PM
             xFormat: '%B %-d, %Y at %-I:%M%p', // 'xFormat' can be used as custom format of 'x'
             columns: plotData
         },
+        padding: {
+            left: 40,
+            right: 40,
+            top: 20
+        },
         axis: {
             x: {
                 type: 'timeseries',
                 tick: {
                     rotate: 15,
-                    format: "%H:%M:%S %Y-%m-%d"
+                    format: "%H:%M:%S %m-%d-%Y"
                 }
             }
         },
         zoom: {
             enabled: true
+        }
+    });
+}
+function generateDayOfWeekPlot(divID, dayOfWeekCounts){
+    c3.generate({
+        bindto: divID + " .plot-box",
+        data: {
+            json: dayOfWeekCounts,
+            type : 'donut',
+        },
+        donut: {
+            title: "Workout DoW"
         }
     });
 }
